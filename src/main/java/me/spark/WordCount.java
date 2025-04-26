fba9bf813579550450bcd6c5d9b0b2dc5532abb3
kheder-hassoun <khdrhswn32@gmail.com>
2025-04-26 21:53:06 +0300

    Docker compose every thing is dockerized fix dependencies and build problem

--- src/main/java/me/spark/WordCount.java
+++ src/main/java/me/spark/WordCount.java
@@ -1,24 +1,29 @@
 package me.spark;
 
-
 import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.sql.*;
 import static org.apache.spark.sql.functions.*;
 
 public class WordCount {
     public static void main(String[] args) {
+        if (args.length < 2) {
+            System.err.println("Usage: WordCount <inputPath> <outputPath>");
+            System.exit(1);
+        }
+
         SparkSession spark = SparkSession.builder()
                 .appName("Word Count Pipeline")
-                .master("local[*]") // Use all local cores
+                // Remove .master("local[*]") to use cluster mode
                 .getOrCreate();
 
-        // Load raw text file
-        Dataset<String> lines = spark.read().textFile("input_data/documents.txt");
+        // Load raw text file from HDFS
+        Dataset<String> lines = spark.read().textFile(args[0]);
 
         // Clean and split into words
         Dataset<String> words = lines
                 .flatMap((FlatMapFunction<String, String>) line ->
-                        java.util.Arrays.asList(line.toLowerCase().split("\\W+")).iterator(), Encoders.STRING());
+                                java.util.Arrays.asList(line.toLowerCase().split("\\W+")).iterator(),
+                        Encoders.STRING());
 
         // Group and count
         Dataset<Row> wordCounts = words.groupBy("value").count().orderBy(desc("count"));
@@ -26,8 +31,11 @@ public class WordCount {
         // Show sample
         wordCounts.show();
 
-        // Save as key-value pairs (text file output or DB write here)
-        wordCounts.write().format("csv").mode("overwrite").save("output_data/word_counts");
+        // Save output to HDFS
+        wordCounts.write()
+                .format("csv")
+                .mode("overwrite")
+                .save(args[1]);
 
         spark.stop();
     }
