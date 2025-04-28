67174c8ba03728e0e418e5e603202ffdb73e7b71
kheder-hassoun <khdrhswn32@gmail.com>
2025-04-28 23:29:01 +0300

    mongoDb worked (just a test)

--- src/main/java/me/spark/WordCount.java
+++ src/main/java/me/spark/WordCount.java
@@ -6,37 +6,62 @@ import static org.apache.spark.sql.functions.*;
 
 public class WordCount {
     public static void main(String[] args) {
-        if (args.length < 2) {
-            System.err.println("Usage: WordCount <inputPath> <outputPath>");
-            System.exit(1);
-        }
-
         SparkSession spark = SparkSession.builder()
-                .appName("Word Count Pipeline")
-                // Remove .master("local[*]") to use cluster mode
+                .appName("MongoSparkConnector")
+                // More specific configuration
+                .config("spark.mongodb.read.connection.uri", "mongodb://mongodb:27017")
+                .config("spark.mongodb.write.connection.uri", "mongodb://mongodb:27017")
+                .config("spark.mongodb.database", "mymongo")
+                .config("spark.mongodb.collection", "testcollection")
                 .getOrCreate();
 
-        // Load raw text file from HDFS
-        Dataset<String> lines = spark.read().textFile(args[0]);
+        // Read from MongoDB
+        Dataset<Row> df = spark.read()
+                .format("mongodb")
+                .load();
 
-        // Clean and split into words
-        Dataset<String> words = lines
-                .flatMap((FlatMapFunction<String, String>) line ->
-                                java.util.Arrays.asList(line.toLowerCase().split("\\W+")).iterator(),
-                        Encoders.STRING());
+        df.show();
 
-        // Group and count
-        Dataset<Row> wordCounts = words.groupBy("value").count().orderBy(desc("count"));
-
-        // Show sample
-        wordCounts.show();
-
-        // Save output to HDFS
-        wordCounts.write()
-                .format("csv")
-                .mode("overwrite")
-                .save(args[1]);
-
-        spark.stop();
+        // Write to MongoDB
+        // ... your DataFrame operations ...
+        // df.write()
+        //    .format("mongodb")
+        //    .mode("overwrite")
+        //    .save();
     }
+
+//    public static void main(String[] args) {
+//        if (args.length < 2) {
+//            System.err.println("Usage: WordCount <inputPath> <outputPath>");
+//            System.exit(1);
+//        }
+//
+//        SparkSession spark = SparkSession.builder()
+//                .appName("Word Count Pipeline")
+//                // Remove .master("local[*]") to use cluster mode
+//                .getOrCreate();
+//
+//        // Load raw text file from HDFS
+//        Dataset<String> lines = spark.read().textFile(args[0]);
+//
+//        // Clean and split into words
+//        Dataset<String> words = lines
+//                .flatMap((FlatMapFunction<String, String>) line ->
+//                                java.util.Arrays.asList(line.toLowerCase().split("\\W+")).iterator(),
+//                        Encoders.STRING());
+//
+//        // Group and count
+//        Dataset<Row> wordCounts = words.groupBy("value").count().orderBy(desc("count"));
+//
+//        // Show sample
+//        wordCounts.show();
+//
+//        // Save output to HDFS
+//        wordCounts.write()
+//                .format("csv")
+//                .mode("overwrite")
+//                .save(args[1]);
+//
+//        spark.stop();
+//    }
 }
